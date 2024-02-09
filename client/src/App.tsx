4692fa1d496bbd9a78c0d7cca383c50ac7e8ba30
suizzle <andygolay@gmail.com>
2024-02-09 11:38:41 -0500

    Fetching bio successfully

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useWallet, InputTransactionData } from '@aptos-labs/wallet-adapter-react';
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 import { ONCHAIN_BIO } from "./constants";
@@ -11,9 +11,39 @@ const aptosConfig = new AptosConfig({ network: Network.TESTNET });
 const aptos = new Aptos(aptosConfig);
 
 function App() {
-  const { signAndSubmitTransaction } = useWallet();
+  const { signAndSubmitTransaction, account } = useWallet();
   const name = useRef<HTMLInputElement>(null);
   const bio = useRef<HTMLTextAreaElement>(null);
+
+  const [accountHasBio, setAccountHasBio] = useState(false);
+  const [currentName, setCurrentName] = useState(null);
+  const [currentBio, setCurrentBio] = useState(null);
+
+  const fetchBio = async () => {
+    if (!account) {
+      console.log("No account")
+      return [];
+    }
+  
+    try {
+      const bioResource = await aptos.getAccountResource(
+        {
+          accountAddress:account?.address,
+          resourceType:`${ONCHAIN_BIO}::onchain_bio::Bio`
+        }
+      );
+      console.log("Name:", bioResource.name, "Bio:", bioResource.bio);
+      setAccountHasBio(true);
+      if (bioResource) {
+        setCurrentName(bioResource.name);
+        setCurrentBio(bioResource.bio);
+      } else {
+        console.log("no bio")
+      }
+    } catch (e: any) {
+      setAccountHasBio(false);
+    }
+  };
 
   async function registerBio() {
     if (bio.current !== null && name.current !== null) { 
@@ -29,8 +59,9 @@ function App() {
         // sign and submit transaction to chain
         const response = await signAndSubmitTransaction(transaction);
         // wait for transaction
-        console.log(response)
+        console.log(`Success! View your transaction at https://explorer.aptoslabs.com/txn/${response.hash}`)
         await aptos.waitForTransaction({transactionHash:response.hash});
+        fetchBio();
       } catch (error: any) {
         console.log("Error:", error)
       }
@@ -68,6 +99,25 @@ function App() {
 
         <div className="row">
           <button onClick={registerBio}>Register Bio</button>
+        </div>
+
+        <div className="row">
+          <button onClick={fetchBio}>Fetch Bio</button>
+        </div>
+
+
+        <div className="row">
+          <center>
+            <h3>Your Name:</h3>
+            <p>{currentName}</p>
+          </center>
+        </div>
+
+        <div className="row">
+          <center>
+            <h3>Your Bio:</h3>
+            <p>{currentBio}</p>
+          </center>
         </div>
 
       </div>
